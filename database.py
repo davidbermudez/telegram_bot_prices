d9e5ddaadd54017cd652399518fad826e0de20d9
davidbermudez <davidbermudez@jerez.es>
2026-07-26 16:42:58 +0200

    rutas absolutas archivo sqlite

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
+import os
 import sqlite3
 
-DB_PATH = "precios.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+DB_PATH = os.path.join(
+    BASE_DIR,
+    "precios.db"
+)
 
 def conectar():
 
